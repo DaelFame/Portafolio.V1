00e0d01032087132ea76348ab1157602e95fcea1
DaelFame <fotos2968@gmail.com>
2025-06-27 12:38:35 -0500

    Cambio para site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap"; // 👈 agregá esta línea
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://joacoportfolioanalis.netlify.app', // 👈 este debe ser tu dominio real publicado
+  integrations: [tailwind(), robotsTxt(), sitemap()], // 👈 agregá sitemap aquí
+});
