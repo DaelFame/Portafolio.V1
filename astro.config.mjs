ee0c4dad6ff843b2253139c635af0b999f305f48
Joaquin Cardenas <joaquin.data.dev@gmail.com>
2026-03-27 01:10:50 -0500

    fix: clean astro config and correct css syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap"; // 👈 agregá esta línea
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://joacoportfolioanalis.netlify.app', // 👈 este debe ser tu dominio real publicado
-  integrations: [tailwind(), robotsTxt(), sitemap()], // 👈 agregá sitemap aquí
+  site: 'https://joacoportfolioanalis.netlify.app',
+  integrations: [tailwind(), robotsTxt(), sitemap()],
 });
