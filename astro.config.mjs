7f0f1efc99aeba5d3c81c2f56c935950dc55fd76
Joaquin Cardenas <joaquin.data.dev@gmail.com>
2026-03-27 01:15:05 -0500

    debug: removing sitemap plugin to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://jcardenas-financial-data.netlify.app',
-  integrations: [tailwind(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), robotsTxt()],
 });
